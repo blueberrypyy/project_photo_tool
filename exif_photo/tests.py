868c8dbd25e475f464478cb6d10b2c266a16e379
Jay <blueberry.py@protonmail.com>
2024-04-04 21:51:33 -0600

    Updated tests

--- exif_photo/tests.py
+++ exif_photo/tests.py
@@ -1,11 +1,28 @@
 from django.test import TestCase
 from django.test import SimpleTestCase
-import requests
 
-class homepage_tests(SimpleTestCase):
 
+class home_page_tests(SimpleTestCase):
     def check_home_page_returns_200(self):
         response = self.client.get('/')
         self.response.assertEquals(response, 200)
 
+class exif_page_test(SimpleTestCase):
+    def check_home_page_returns_200(self):
+            response = self.client.get('/exif')
+            self.response.assertEquals(response, 200)
 
+class dotmethod_page_test(SimpleTestCase):
+    def check_home_page_returns_200(self):
+            response = self.client.get('/dotmethod')
+            self.response.assertEquals(response, 200)
+
+class address_page_test(SimpleTestCase):
+    def check_home_page_returns_200(self):
+            response = self.client.get('/address_edit')
+            self.response.assertEquals(response, 200)
+
+class about_page_test(SimpleTestCase):
+    def check_home_page_returns_200(self):
+            response = self.client.get('/about')
+            self.response.assertEquals(response, 200)
